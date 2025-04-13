3bd6bd6ee24cda6e75fb8c18f2265d1d7cc3afd4
leovanillajs <leovanillajs@gmail.com>
2025-04-12 23:01:28 -0300

    Usando o componente criado mais de uma vez, porém existe um problema que é  a repetição da foto da Emma.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,24 @@ function App() {
         <div className="caixa-principal">
           <h1>Here is what our alumni say about freeCodeCamp:</h1>
           <Testimony 
+                nome='Shawn Wang'
+                pais='Singapore'
+                imagem=''
+                cargo='Software Engineer'
+                empresa='Amazon'
+                testimony="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+          />
+
+          <Testimony 
+                nome='Sarah Chima'
+                pais='Nigeria'
+                imagem=''
+                cargo='Software Engineer'
+                empresa='ChatDesk'
+                testimony="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+          />
+          
+          <Testimony 
                 nome='Emma Bostian'
                 pais='Suecia'
                 imagem=''
