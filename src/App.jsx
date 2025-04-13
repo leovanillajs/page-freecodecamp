0696d54a46782c6f4f5581c38c6ad3b44fbae590
leovanillajs <leovanillajs@gmail.com>
2025-04-12 21:43:51 -0300

    4) Usar e passar props a um componente.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,14 @@ function App() {
       <div className='App'>
         <div className="caixa-principal">
           <h1>Here is what our alumni say about freeCodeCamp:</h1>
-          <Testimony />
+          <Testimony 
+                nome='Emma Bostian'
+                pais='Suecia'
+                imagem=''
+                cargo='Software Engineer'
+                empresa='Spotify'
+                testimony="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+          />
         </div>        
       </div>
     </>
--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Foto from '../imagens/testimonio-emma.png'
 import '../stylesheets/Testimony.css'
 
-function Testimony() {
+function Testimony(props) {
     return (
         <div className='caixa-testimony'>
             <img 
@@ -11,11 +11,9 @@ function Testimony() {
                 alt='Foto de Emma'
             />
             <div className="caixa-texto-testimony">
-                <p className="nome-testimony">Emma Bostian en Suecia</p>
-                <p className="cargo-testimony">Software Engineer at Spotify</p>
-                <p className="texto-testimony">
-                    "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
-                </p>
+                <p className="nome-testimony">{props.nome} en {props.pais}</p>
+                <p className="cargo-testimony">{props.cargo} at {props.empresa}</p>
+                <p className="texto-testimony">"{props.testimony}"</p>
             </div>
         </div>
     )
